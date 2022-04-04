996c4069f67aad29ab2a6ad4acfb3c364cb83170
haiphan5289 <haiphan5289@gmail.com>
2022-04-04 09:09:54 +0700

    add tabbar

---
+++ EasyAudio/Modules/Tabbar/TabbarVC.swift
@@ -0,0 +1,29 @@
+//
+//  TabbarVC.swift
+//  EasyAudio
+//
+//  Created by haiphan on 03/04/2022.
+//
+
+import UIKit
+
+class TabbarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
